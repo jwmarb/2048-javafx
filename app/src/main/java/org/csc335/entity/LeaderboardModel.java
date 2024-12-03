8f1d09ae1857968e2a22c11fb958488504dc23c5
CoolPenguin42 <nemesist06@gmail.com>
2024-12-02 23:04:27 -0700

    feat: leaderboardModel mostly fixed to use priorityQueue and with a fixed size instead

--- app/src/main/java/org/csc335/entity/LeaderboardModel.java
+++ app/src/main/java/org/csc335/entity/LeaderboardModel.java
@@ -1,6 +1,10 @@
 package org.csc335.entity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
@@ -8,52 +12,43 @@ import java.io.BufferedWriter;
 
 public class LeaderboardModel {
 
-  // instance variables
-  private ArrayList<Integer> scores;
+  private final PriorityQueue<Integer> maxHeap;
 
-  private String top10;
-
-  private static String FILE_NAME = "leaderboard.txt";
+  private static final String FILE_NAME = "leaderboard.txt";
 
   public LeaderboardModel() {
-    scores = new ArrayList<>();
+    maxHeap = new PriorityQueue<Integer>(10){
+      private final int maxSize = 10;
+
+      @Override
+      public boolean add(Integer e) {
+          if (size() < maxSize) {
+              return super.add(e);
+          } else {
+              if (comparator().compare(e, peek()) > 0) { // Assuming a max heap
+                  poll(); // Remove the smallest element
+                  return super.add(e);
+              } else {
+                  return false; // Element not added
+              }
+          }
+      }
+    };
   }
 
   public String load() {
-    // ArrayList<Player> top5 = new ArrayList<>();
-    this.top10 = "";
-    int count = 10;
-    for (int i = 0; i < scores.size(); i++) {
-      int score = scores.get(i);
-      if (count > 0) {
-        this.top10 += String.format("%d.    %d points\n", i + 1, score);
-        count--;
-      }
+    String top10 = "";
+
+    int c = 0;
+    Object[] t = maxHeap.toArray();
+    Arrays.sort(t, Collections.reverseOrder());
+    for (Object score : t) {
+      top10 += String.format("%d.    %d points\n", ++c, (int)score);
     }
 
     return top10;
   }
 
-  // helper method
-  public void addNewScore(int newScore) {
-    if (scores.size() == 0) {
-      scores.add(newScore);
-    } else {
-      for (int i = 0; i < this.scores.size(); i++) {
-        // System.out.println(scores.get(i));
-        if (scores.get(i) < newScore) {
-          scores.add(i, newScore);
-          break;
-        }
-        if (i == this.scores.size() - 1) {
-          scores.add(newScore);
-          break;
-        }
-      }
-    }
-  }
-
-  // adds players from leaderboard file
   public void addScores() {
     File file = new File(FILE_NAME);
     Scanner fileReader = null;
@@ -63,7 +58,7 @@ public class LeaderboardModel {
 
       while (fileReader.hasNext()) {
         int score = Integer.parseInt(fileReader.nextLine().trim());
-        addNewScore(score);
+        maxHeap.add(score);
       }
 
       fileReader.close();
@@ -76,18 +71,22 @@ public class LeaderboardModel {
 
   // write in file
   public void writeNewPlayerScore(int newScore) {
+    System.out.println(maxHeap.size());
     try {
 
       // get the saved data
       addScores();
-      System.out.println(scores);
-      addNewScore(newScore);
-      System.out.println(scores);
+      System.out.println(maxHeap);
+
+      maxHeap.add(newScore);
+      //addNewScore(newScore);
+      System.out.println(maxHeap);
 
       BufferedWriter fileWriter = new BufferedWriter(new FileWriter(FILE_NAME));
-
-      for (int score : scores) {
-        fileWriter.write(String.format("%d\n", score));
+      Object[] t = maxHeap.toArray();
+      Arrays.sort(t, Collections.reverseOrder());
+      for (Object score : t) {
+        fileWriter.write(String.format("%d\n", (int)score));
       }
 
       fileWriter.close();
