75aeb9f9036ec6fa0b3b340fc866d0e15a91886d
CoolPenguin42 <nemesist06@gmail.com>
2024-12-06 15:35:51 -0700

    chore: timermodeltest created

---
+++ app/src/test/java/org/csc335/model_tests/TimerModelTest.java
@@ -0,0 +1,14 @@
+package org.csc335.model_tests;
+
+import org.csc335.entity.GameMode;
+import org.csc335.interfaces.DrawerOptionListener;
+import org.csc335.models.DrawerMenuModel;
+import org.junit.Test;
+
+import javafx.beans.property.SimpleBooleanProperty;
+import java.lang.reflect.Field;
+
+
+public class TimerModelTest {
+    
+}
