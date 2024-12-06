c79e054c71c293f53cde4352888d6f616f1d288b
Joseph Marbella <dieptuber@gmail.com>
2024-12-06 16:24:04 -0700

    chore: add comments

--- app/src/main/java/org/csc335/models/GameModel.java
+++ app/src/main/java/org/csc335/models/GameModel.java
@@ -6,6 +6,11 @@ import java.util.List;
 import org.csc335.entity.GameMode;
 import org.csc335.interfaces.GameListener;
 
+/**
+ * Represents the model component of the 2048 game, managing the game's state
+ * and logic. This class holds the current game mode. It provides methods to
+ * update and retrieve the game mode, and notifies listeners of those changes.
+ */
 public class GameModel {
   private List<GameListener> listeners;
   private GameMode gameMode;
@@ -15,21 +20,44 @@ public class GameModel {
     this.setGameMode(GameMode.TRADITIONAL);
   }
 
+  /**
+   * Sets the game mode for the current game session.
+   *
+   * @post The gameMode of the current game session has been updated to the
+   *       specified mode, and all dependent components have been notified of the
+   *       change.
+   *
+   * @param mode The new game mode to be set for the game session.
+   */
   public void setGameMode(GameMode mode) {
     this.gameMode = mode;
     this.notifyDependencies();
   }
 
+  /**
+   * Retrieves the current game mode of the game.
+   *
+   * @return The current game mode as an instance of GameMode.
+   */
   public GameMode getGameMode() {
     return this.gameMode;
   }
 
+  /**
+   * Notifies all registered GameListeners about a change in the game mode.
+   */
   private void notifyDependencies() {
     for (GameListener listener : this.listeners) {
       listener.gameModeChanged(this.gameMode);
     }
   }
 
+  /**
+   * Adds a GameListener to the list of listeners.
+   *
+   * @post The listener is added to the list of listeners.
+   * @param listener The GameListener to be added.
+   */
   public void addListener(GameListener listener) {
     this.listeners.add(listener);
   }
