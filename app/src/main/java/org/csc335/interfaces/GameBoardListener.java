cffa03ee39db7792a67363c46f4e3ae77486d0da
Joseph Marbella <dieptuber@gmail.com>
2024-12-06 15:35:13 -0700

    refactor: use model

--- app/src/main/java/org/csc335/interfaces/GameBoardListener.java
+++ app/src/main/java/org/csc335/interfaces/GameBoardListener.java
@@ -1,9 +1,12 @@
 package org.csc335.interfaces;
 
+import org.csc335.models.TileModel;
+
 public interface GameBoardListener {
   public void scoreChanged(int diff);
 
   public void gameOver();
 
   public void tileMoved();
+
 }
