cc10a569f8abbc0332bb72d5eb99a4cedd0d9c56
CoolPenguin42 <nemesist06@gmail.com>
2024-12-06 13:01:51 -0700

    feat: DrawerMenuModelTest some tests added

--- app/src/test/java/org/csc335/model_tests/DrawerMenuModelTest.java
+++ app/src/test/java/org/csc335/model_tests/DrawerMenuModelTest.java
@@ -1,11 +1,38 @@
 package org.csc335.model_tests;
 
+import org.csc335.entity.GameMode;
 import org.csc335.models.DrawerMenuModel;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import static org.junit.Assert.*;
 
 public class DrawerMenuModelTest {
+
+    @Test
+    public void testSelection() {
+        DrawerMenuModel drawer = new DrawerMenuModel();
+
+        for (GameMode g : GameMode.values()) {
+            drawer.setSelected(g);
+            assert (drawer.getSelected().get() == g);
+        }
+    }
+
+    @Test
+    public void testSelectionOrdinal() {
+        DrawerMenuModel drawer = new DrawerMenuModel();
+
+        assert(drawer.getSelectedOrdinal().isEmpty());
+
+        for (GameMode g : GameMode.values()) {
+            drawer.setSelected(g);
+            assert (drawer.getSelectedOrdinal().get() == g.ordinal());
+        }
+    }
+
     
 }
