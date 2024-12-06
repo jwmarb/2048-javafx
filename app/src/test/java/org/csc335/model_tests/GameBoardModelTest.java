6fa931b5e4a07e57c570c56999157a64969cc9c0
CoolPenguin42 <nemesist06@gmail.com>
2024-12-06 16:31:40 -0700

    fix: GameBoardModel fix, and testfile created

---
+++ app/src/test/java/org/csc335/model_tests/GameBoardModelTest.java
@@ -0,0 +1,37 @@
+package org.csc335.model_tests;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import org.csc335.entity.LeaderboardModel;
+import org.csc335.models.GameBoardModel;
+import org.junit.Test;
+
+public class GameBoardModelTest {
+    
+    @Test
+    public void testReset() {
+
+    }
+
+        
+    @Test
+    public void testAddNewTiles() {
+        GameBoardModel game = new GameBoardModel(4);
+        
+        for (int i = 0; i < 14; i++) {
+
+        }
+        
+    }
+
+    @Test
+    public void testGameFunction() {
+        
+    }
+}
