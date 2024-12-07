77cbf97b91848f35cf4798e1a5467524416f9b6d
Joseph Marbella <dieptuber@gmail.com>
2024-12-06 19:26:25 -0700

    fix: test import

--- app/src/test/java/org/csc335/model_tests/GameBoardModelTest.java
+++ app/src/test/java/org/csc335/model_tests/GameBoardModelTest.java
@@ -13,146 +13,144 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Stream;
 
+import org.csc335.entity.Direction;
 import org.csc335.models.GameBoardModel;
 import org.csc335.models.LeaderboardModel;
 import org.csc335.models.TileModel;
 import org.junit.jupiter.api.Test;
 
-import javafx.scene.control.skin.TextInputControlSkin.Direction;
-
 public class GameBoardModelTest {
 
-    @Test
-    public void testReset() {
+  @Test
+  public void testReset() {
+
+  }
+
+  @Test
+  public void testAddNewTiles() {
+    GameBoardModel game = new GameBoardModel(4);
+
+    for (int i = 0; i < 14; i++) {
 
     }
 
-    @Test
-    public void testAddNewTiles() {
-        GameBoardModel game = new GameBoardModel(4);
+  }
 
-        for (int i = 0; i < 14; i++) {
+  @Test
+  public void testGameFunction() {
+    GameBoardModel game = new GameBoardModel(4);
+    TileModel[][] actualBoard = forceGameStartState(game);
+    printDebugHelper(game);
 
-        }
+    game.reset();
+    printDebugHelper(game);
+  }
 
+  @Test
+  public void testBlankTiles() {
+    GameBoardModel game = new GameBoardModel(4); // should be 14 blank tiles
+
+    updateBlankTiles(game);
+
+    List<TileModel> emptyTiles = getEmptyTileList(game);
+
+    assert (emptyTiles.size() == 14);
+
+    for (int i = 13; i >= 0; i--) {
+      updateBlankTiles(game);
+      generateRandomValues(game);
+      assertEquals(i, emptyTiles.size() - 1);
+    }
+  }
+
+  @Test
+  public void shiftTest() {
+    GameBoardModel model = new GameBoardModel(4);
+
+    try {
+      Method method = GameBoardModel.class.getDeclaredMethod("shift", Direction.class);
+      method.setAccessible(true);
+
+      // testing the shifts
+
+      for (Direction direction : Direction.values()) {
+        assert (boolean) method.invoke(model, direction) == true;
+        System.out.println("success");
+      }
+
+      assert (boolean) method.invoke(model, Direction.valueOf("abc")) == false;
+
+      /*
+       * assertTrue((boolean)method.invoke(model, Direction.RIGHT));
+       * assertTrue((boolean)method.invoke(model, Direction.LEFT));
+       * assertTrue((boolean)method.invoke(model, Direction.UP));
+       * assertTrue((boolean)method.invoke(model, Direction.DOWN));
+       * assertFalse((boolean)method.invoke(model, Direction.valueOf("abc")));
+       */
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private List<TileModel> getEmptyTileList(GameBoardModel game) {
+    List<TileModel> emptyTiles = null;
+    try {
+      Field field = GameBoardModel.class.getDeclaredField("emptyTiles");
+      field.setAccessible(true);
+      emptyTiles = (List<TileModel>) field.get(game);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return emptyTiles;
+  }
+
+  private void updateBlankTiles(GameBoardModel game) {
+    try {
+      Method method = GameBoardModel.class.getDeclaredMethod("updateBlankTiles");
+      method.setAccessible(true);
+      method.invoke(game);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void generateRandomValues(GameBoardModel game) {
+    try {
+      Method method = GameBoardModel.class.getDeclaredMethod("generateRandomValues");
+      method.setAccessible(true);
+      method.invoke(game);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private TileModel[][] forceGameStartState(GameBoardModel game) {
+    TileModel[][] gameBoard = null;
+    try {
+      Field field = GameBoardModel.class.getDeclaredField("board");
+      field.setAccessible(true);
+      gameBoard = (TileModel[][]) field.get(game);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    for (int i = 0; i < gameBoard.length; ++i) {
+      for (int j = 0; j < gameBoard[i].length; ++j) {
+        gameBoard[i][j].makeBlank();
+      }
     }
 
-    @Test
-    public void testGameFunction() {
-        GameBoardModel game = new GameBoardModel(4);
-        TileModel[][] actualBoard = forceGameStartState(game);
-        printDebugHelper(game);
+    return gameBoard;
+  }
 
-        game.reset();
-        printDebugHelper(game);
+  private void printDebugHelper(GameBoardModel game) {
+    try {
+      Method method = GameBoardModel.class.getDeclaredMethod("printBoard");
+      method.setAccessible(true);
+      method.invoke(game);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
-    @Test
-    public void testBlankTiles() {
-        GameBoardModel game = new GameBoardModel(4); // should be 14 blank tiles
-
-        updateBlankTiles(game);
-
-        List<TileModel> emptyTiles = getEmptyTileList(game);
-
-        assert (emptyTiles.size() == 14);
-
-        for (int i = 13; i >= 0; i--) {
-            updateBlankTiles(game);
-            generateRandomValues(game);
-            assertEquals(i, emptyTiles.size() - 1);
-        }
-    }
-
-    @Test
-    public void shiftTest() {
-        GameBoardModel model = new GameBoardModel(4);
-
-
-        try {
-            Method method = GameBoardModel.class.getDeclaredMethod("shift", Direction.class);
-            method.setAccessible(true);
-
-            // testing the shifts
-
-            for (Direction direction : Direction.values()) {
-                assert (boolean)method.invoke(model, direction) == true;
-                System.out.println("success");
-            }
-
-            assert (boolean)method.invoke(model, Direction.valueOf("abc")) == false;
-
-            /* 
-            assertTrue((boolean)method.invoke(model, Direction.RIGHT));
-            assertTrue((boolean)method.invoke(model, Direction.LEFT));
-            assertTrue((boolean)method.invoke(model, Direction.UP));
-            assertTrue((boolean)method.invoke(model, Direction.DOWN));
-            assertFalse((boolean)method.invoke(model, Direction.valueOf("abc")));
-            */
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private List<TileModel> getEmptyTileList(GameBoardModel game) {
-        List<TileModel> emptyTiles = null;
-        try {
-            Field field = GameBoardModel.class.getDeclaredField("emptyTiles");
-            field.setAccessible(true);
-            emptyTiles = (List<TileModel>) field.get(game);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return emptyTiles;
-    }
-
-    private void updateBlankTiles(GameBoardModel game) {
-        try {
-            Method method = GameBoardModel.class.getDeclaredMethod("updateBlankTiles");
-            method.setAccessible(true);
-            method.invoke(game);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void generateRandomValues(GameBoardModel game) {
-        try {
-            Method method = GameBoardModel.class.getDeclaredMethod("generateRandomValues");
-            method.setAccessible(true);
-            method.invoke(game);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private TileModel[][] forceGameStartState(GameBoardModel game) {
-        TileModel[][] gameBoard = null;
-        try {
-            Field field = GameBoardModel.class.getDeclaredField("board");
-            field.setAccessible(true);
-            gameBoard = (TileModel[][]) field.get(game);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        for (int i = 0; i < gameBoard.length; ++i) {
-            for (int j = 0; j < gameBoard[i].length; ++j) {
-                gameBoard[i][j].makeBlank();
-            }
-        }
-
-        return gameBoard;
-    }
-
-    private void printDebugHelper(GameBoardModel game) {
-        try {
-            Method method = GameBoardModel.class.getDeclaredMethod("printBoard");
-            method.setAccessible(true);
-            method.invoke(game);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(game.getMoves());
-    }
+    System.out.println(game.getMoves());
+  }
 }
