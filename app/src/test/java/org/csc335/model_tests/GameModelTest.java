6787f216724456869bba20db3e7aee8baf7b16c6
joshjackson1494 <joshuajackson1494@gmail.com>
2024-12-06 18:33:07 -0700

    feat: Created and finished GameModelTest file with 100 percent coverage

---
+++ app/src/test/java/org/csc335/model_tests/GameModelTest.java
@@ -0,0 +1,53 @@
+package org.csc335.model_tests;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.time.Duration;
+
+import org.csc335.entity.GameMode;
+import org.csc335.interfaces.GameListener;
+import org.csc335.models.GameModel;
+import org.csc335.models.TimerModel;
+import org.junit.jupiter.api.Test;
+
+public class GameModelTest {
+
+    @Test
+    public void testGameMode() {
+        GameModel model = new GameModel();
+
+        model.setGameMode(GameMode.TRADITIONAL);
+
+        assert (model.getGameMode() == GameMode.TRADITIONAL);
+
+        model.setGameMode(GameMode.TIME_TRIAL);
+
+        assert (model.getGameMode() == GameMode.TIME_TRIAL);
+    }
+
+    GameMode mode = GameMode.TRADITIONAL;
+
+    @Test
+    public void testNotifyDependenciesAndAddListener() {
+        GameModel model = new GameModel();
+
+        GameListener listener = new GameListener() {
+            public void gameModeChanged(GameMode newMode) {
+                mode = newMode;
+            }
+        };
+
+        model.addListener(listener);
+        model.setGameMode(GameMode.TIME_TRIAL);
+
+        try {
+            Method method = GameModel.class.getDeclaredMethod("notifyDependencies");
+            method.setAccessible(true);
+            method.invoke(model);
+
+            assert (mode == GameMode.TIME_TRIAL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
