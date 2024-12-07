4a719c99a53185a83cac4b75b96efac74c1b8f01
Joseph Marbella <dieptuber@gmail.com>
2024-12-06 20:38:24 -0700

    tests: add test for setIsDone

--- app/src/test/java/org/csc335/model_tests/TimerModelTest.java
+++ app/src/test/java/org/csc335/model_tests/TimerModelTest.java
@@ -8,118 +8,127 @@ import org.csc335.models.TimerModel;
 import org.junit.jupiter.api.Test;
 
 import javafx.beans.property.SimpleBooleanProperty;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.time.Duration;
 
 public class TimerModelTest {
-    @Test
-    public void testDecrementSecondAndGetTimeLeft() {
-        TimerModel model = new TimerModel();
+  @Test
+  public void testDecrementSecondAndGetTimeLeft() {
+    TimerModel model = new TimerModel();
 
-        assert (model.getTimeLeft().toMinutes() == 15);
+    assert (model.getTimeLeft().toMinutes() == 15);
 
-        for (int i = 0; i < 60; ++i) {
-            try {
-                Method method = TimerModel.class.getDeclaredMethod("decrementSecond");
-                method.setAccessible(true);
-                method.invoke(model);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        assert (model.getTimeLeft().toMinutes() == 14);
-
+    for (int i = 0; i < 60; ++i) {
+      try {
+        Method method = TimerModel.class.getDeclaredMethod("decrementSecond");
+        method.setAccessible(true);
+        method.invoke(model);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
 
-    @Test
-    public void testIsDone() {
-        TimerModel model = new TimerModel();
+    assert (model.getTimeLeft().toMinutes() == 14);
 
-        assert (model.isDone());
+  }
 
-        try {
-            Field field = TimerModel.class.getDeclaredField("isDone");
-            field.setAccessible(true);
-            field.set(model, false);
-            assert (!model.isDone());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+  @Test
+  public void testIsDone() {
+    TimerModel model = new TimerModel();
 
+    assert (model.isDone());
+
+    try {
+      Field field = TimerModel.class.getDeclaredField("isDone");
+      field.setAccessible(true);
+      field.set(model, false);
+      assert (!model.isDone());
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
-    @Test
-    public void testReset() {
-        TimerModel model = new TimerModel();
+  }
 
-        try {
-            Field field = TimerModel.class.getDeclaredField("duration");
-            field.setAccessible(true);
-            field.set(model, Duration.ofMinutes(10));
-            assert (model.getTimeLeft().toMinutes() == 10);
+  @Test
+  public void testReset() {
+    TimerModel model = new TimerModel();
 
-            model.reset();
+    try {
+      Field field = TimerModel.class.getDeclaredField("duration");
+      field.setAccessible(true);
+      field.set(model, Duration.ofMinutes(10));
+      assert (model.getTimeLeft().toMinutes() == 10);
 
-            assert (model.getTimeLeft().toMinutes() == 15);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+      model.reset();
+
+      assert (model.getTimeLeft().toMinutes() == 15);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  Duration time = Duration.ofMinutes(15);
+
+  @Test
+  public void testHandleDurationChangeAndAddListener() {
+    TimerModel model = new TimerModel();
+
+    TimerListener listener = new TimerListener() {
+      public void timerChanged(Duration timeLeft) {
+        time = timeLeft;
+      }
+    };
+    model.addListener(listener);
+
+    try {
+      Field field = TimerModel.class.getDeclaredField("duration");
+      field.setAccessible(true);
+      field.set(model, Duration.ofMinutes(10));
+
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
-    Duration time = Duration.ofMinutes(15);
-
-    @Test
-    public void testHandleDurationChangeAndAddListener() {
-        TimerModel model = new TimerModel() {
-            @Override
-            public void stop() {
-
-            }
-        };
-
-        TimerListener listener = new TimerListener() {
-            public void timerChanged(Duration timeLeft) {
-                time = timeLeft;
-            }
-        };
-        model.addListener(listener);
-
-        try {
-            Field field = TimerModel.class.getDeclaredField("duration");
-            field.setAccessible(true);
-            field.set(model, Duration.ofMinutes(10));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try {
-            Method method = TimerModel.class.getDeclaredMethod("handleDurationChange");
-            method.setAccessible(true);
-            method.invoke(model);
-            assert (time.toMinutes() == 10);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try {
-            Field field = TimerModel.class.getDeclaredField("duration");
-            field.setAccessible(true);
-            field.set(model, Duration.ofMinutes(0));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try {
-            Method method = TimerModel.class.getDeclaredMethod("handleDurationChange");
-            method.setAccessible(true);
-            method.invoke(model);
-            assert (time.toMinutes() == 0);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    try {
+      Method method = TimerModel.class.getDeclaredMethod("handleDurationChange");
+      method.setAccessible(true);
+      method.invoke(model);
+      assert (time.toMinutes() == 10);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+
+    try {
+      Field field = TimerModel.class.getDeclaredField("duration");
+      field.setAccessible(true);
+      field.set(model, Duration.ofMinutes(0));
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    try {
+      Method method = TimerModel.class.getDeclaredMethod("handleDurationChange");
+      method.setAccessible(true);
+      method.invoke(model);
+      assert (time.toMinutes() == 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testSetIsDone() {
+    TimerModel model = new TimerModel();
+
+    assertTrue(model.isDone());
+    model.setIsDone(false);
+    assertFalse(model.isDone());
+
+  }
 }
